fca5a9412d71cf15005dc572ca00292b85fbb7a1
안영훈 <wsxc9468@gmail.com>
2020-11-25 12:49:57 +0900

    수류탄 구현

---
+++ FPS_3D/Assets/_PSW/Scripts/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject fxFactory; //이펙트 프리팹
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject fx = Instantiate(fxFactory, transform.position, Quaternion.identity);
+
+
+        Destroy(gameObject);
+           
+            
+    }
+
+
+}
---
+++ FPS_3D/Assets/_PSW/Scripts/PlayerFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bombFactory;
+    public GameObject firePoint;
+    public float power = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject bomb = Instantiate(bombFactory, firePoint.transform.position, Quaternion.identity);
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+
+            // rb.AddForce(Camera.main.transform.forward * power , ForceMode.Impulse);
+            Vector3 dir = Camera.main.transform.forward + (Camera.main.transform.up * 0.5f);
+            dir.Normalize();
+            rb.AddForce(dir * power, ForceMode.Impulse);
+        }
+    }
+}
